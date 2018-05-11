a3ff929ba9a8ca78541c128f4ea76e7b580547ff
Jens Woltering <info@je-wo.com>
2018-05-11 14:36:25 +0200

    Component hinzugefügt

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="test">
+    <h1> It Works</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TestComponent"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+     <test-component></test-component>
   </div>
 </template>
+<script>
+// @ is an alias to /src
+import TestComponent from "@/components/TestComponent";
+
+export default {
+  name: "home",
+  components: {
+    TestComponent
+  }
+};
+</script>

e7550a62ce445c42aa3e52aeedba8e5abd3a497d
Jens Woltering <info@je-wo.com>
2018-05-15 17:46:11 +0200

    ADD Vuetify via cli

--- src/main.js
+++ src/main.js
@@ -2,6 +2,33 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import {
+  Vuetify,
+  VApp,
+  VNavigationDrawer,
+  VFooter,
+  VList,
+  VBtn,
+  VIcon,
+  VGrid,
+  VToolbar,
+  transitions
+} from 'vuetify'
+import '../node_modules/vuetify/src/stylus/app.styl'
+
+Vue.use(Vuetify, {
+  components: {
+    VApp,
+    VNavigationDrawer,
+    VFooter,
+    VList,
+    VBtn,
+    VIcon,
+    VGrid,
+    VToolbar,
+    transitions
+  },
+})
 
 Vue.config.productionTip = false;
 
